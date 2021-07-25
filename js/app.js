86a1fde29c9d0713fb3c31edc99d693775835cd3
IghsaanMurphy <ighsaan.murphy786@gmail.com>
2021-07-25 08:11:50 +0200

    Updates

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const toggle = document.getElementById('toggle'),
+close = document.getElementById('close'),
+open = document.getElementById('open'),
+nav = document.querySelector('nav'),
+modal = document.getElementById('modal');
+
+// TOGGLE NAV 
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
+
+// SHOW MODAL
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal');
+    // CLOSE NAV IF OPEN WHEN SIGNING UP
+    if(document.body.className === 'show-nav'){
+        document.body.classList.remove('show-nav');
+    }
+});
+
+// HIDE MODAL
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// HIDE MODAL BY CLICKING ANYWHERE
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
+
